b3228f7c06a8e28adba4b098dc6449481c7f9e72
James Ostmann <jsostmann@aggies.ncat.edu>
2021-02-11 16:09:47 -0500

    Fixed default Home servlet

---
+++ AggielandSecured/src/main/java/com/aggieland/rest/Home.java
@@ -0,0 +1,17 @@
+package com.aggieland.rest;
+
+import com.aggieland.auth.AuthorizationDAO;
+
+import java.io.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+
+public class Home extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect(request.getContextPath() + "/signin");
+    }
+}
