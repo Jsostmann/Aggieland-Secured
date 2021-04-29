f77b7d194d7a33de732eae0e5852f3aed1f77928
Donald Keith Battle Jr <43556070+dkbattle@users.noreply.github.com>
2021-04-29 11:18:31 -0400

    Update Profile.java

--- AggielandSecured/src/main/java/com/aggieland/rest/Profile.java
+++ AggielandSecured/src/main/java/com/aggieland/rest/Profile.java
@@ -1,9 +1,7 @@
 package com.aggieland.rest;
 
-
 import com.aggieland.model.User;
 import com.aggieland.model.UserDAO;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,10 +13,8 @@ import java.util.ArrayList;
 import java.util.logging.Logger;
 
 public class Profile extends AggielandSecuredServlet{
-
     UserDAO userDAO;
     private static final Logger LOG = Logger.getLogger(Profile.class.getName());
-
 
     @Override
     public void init() throws ServletException {
@@ -27,30 +23,25 @@ public class Profile extends AggielandSecuredServlet{
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
 
-        HttpSession a = request.getSession(false);
-
-        if(a != null && !a.isNew()) {
-            LOG.info("Session is good, continue");
-            LOG.info(String.valueOf(a.getAttribute("user")));
-
+        if(session != null && !session.isNew()) {
+            
+            LOG.info("Valid session continue");
             RequestDispatcher rs = request.getRequestDispatcher("WEB-INF/JSP/profile.jsp");
 
             if(request.getParameter("from-list") != null) {
                 deleteUser(request,response);
             }
 
-            User me = (User)a.getAttribute("user");
+            User me = (User)session.getAttribute("user");
             ArrayList<User> friends = null;
-
+            
             if(me != null) {
-
                 try {
-
                     friends = userDAO.getFriends(me.getUserId());
 
                 } catch (SQLException e) {
-
                     e.printStackTrace();
                 }
             }
@@ -59,37 +50,30 @@ public class Profile extends AggielandSecuredServlet{
             rs.include(request, response);
 
         } else {
-
-            LOG.info("Session is bad, redirect to login");
+            LOG.info("Bad session redirecting to login");
             response.sendRedirect("signin");
         }
-
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
 
-
-        HttpSession a = request.getSession(false);
-
-        if(a != null && !a.isNew()) {
+        if(session != null && !session.isNew()) {
 
             try {
-
                 userDAO.updateAccount(request);
-
-                System.out.println(a.getAttribute("user"));
-
+                
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+            
             doGet(request,response);
             //response.sendRedirect("profile");
 
         } else {
-            System.out.println("bad");
+            LOG.info("Bad session redirecting to login");
             response.sendRedirect("signin");
         }
-
     }
 
     protected void deleteUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
