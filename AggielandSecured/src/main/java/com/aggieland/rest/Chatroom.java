754f872b517c8f93e3327cfdccaf077b296a6883
Donald Keith Battle Jr <43556070+dkbattle@users.noreply.github.com>
2021-04-29 11:09:31 -0400

    Update Chatroom.java

--- AggielandSecured/src/main/java/com/aggieland/rest/Chatroom.java
+++ AggielandSecured/src/main/java/com/aggieland/rest/Chatroom.java
@@ -4,14 +4,11 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-
-
 public class Chatroom extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         RequestDispatcher rs = request.getRequestDispatcher("/WEB-INF/JSP/demochatroom.jsp");
         rs.include(request, response);
-
+        
     }
 }
